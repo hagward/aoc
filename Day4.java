b90fe011969622c62c5cc162cbf14ea244e397c5
Anders Hagward <taperlawyer@gmail.com>
2017-12-05 21:21:03 +0100

    Solve day 4 part 2

--- Day4.java
+++ Day4.java
@@ -3,16 +3,21 @@ import java.util.*;
 
 public class Day4 {
     public static void main(String[] args) throws IOException {
-        int numValid = 0;
+        int numValid1 = 0;
+        int numValid2 = 0;
         try (BufferedReader in = new BufferedReader(new FileReader("Day4Input.txt"))) {
             String line;
             while ((line = in.readLine()) != null) {
                 if (validate(line)) {
-                    numValid += 1;
+                    numValid1 += 1;
+                }
+                if (validate2(line)) {
+                    numValid2 += 1;
                 }
             }
         }
-        System.out.println(numValid);
+        System.out.println(numValid1);
+        System.out.println(numValid2);
     }
 
     private static boolean validate(String passphrase) {
@@ -23,5 +28,25 @@ public class Day4 {
             }
         }
         return true;
+    }
+
+    private static boolean validate2(String passphrase) {
+        Set<Integer> words = new HashSet<>();
+        for (String word : passphrase.split(" ")) {
+            if (!words.add(hash(word))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int hash(String word) {
+        List<Integer> list = new ArrayList<>(26);
+        for (int i = 0; i < 26; i++) list.add(0);
+        for (char c : word.toCharArray()) {
+            int index = c - 97;
+            list.set(index, list.get(index) + 1);
+        }
+        return list.hashCode();
     }
 }
