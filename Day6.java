fd2a9f6dfc6161e79146771baae4d5af98fc467d
Anders Hagward <taperlawyer@gmail.com>
2017-12-06 20:15:49 +0100

    Solve day 6 part 2

--- Day6.java
+++ Day6.java
@@ -2,13 +2,28 @@ import java.util.*;
 
 public class Day6 {
     public static void main(String[] args) {
-        Set<Integer> distributions = new HashSet<>();
         int[] v = {0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11};
+        Set<Integer> distributions = new HashSet<>();
+        Stack<Integer> chain = new Stack<>();
+
         while (distributions.add(Arrays.hashCode(v))) {
-            // System.out.println(Arrays.toString(v));
+            chain.push(Arrays.hashCode(v));
             redistribute(v, max(v));
         }
+
+        int cycles = 1;
+        for (int k = Arrays.hashCode(v); chain.pop() != k; cycles++);
+
         System.out.println(distributions.size());
+        System.out.println(cycles);
+    }
+
+    private static void redistribute(int[] v, int i) {
+        int k = v[i];
+        v[i] = 0;
+        for (int j = 1; j <= k; j++) {
+            v[(i + j) % v.length]++;
+        }
     }
 
     private static int max(int[] v) {
@@ -19,13 +34,5 @@ public class Day6 {
             }
         }
         return max;
-    }
-
-    private static void redistribute(int[] v, int i) {
-        int k = v[i];
-        v[i] = 0;
-        for (int j = 1; j <= k; j++) {
-            v[(i + j) % v.length]++;
-        }
     }
 }
