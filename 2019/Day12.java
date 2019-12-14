77c87ffa54e750f0824572464dbbd7f8114de55c
Anders Hagward <taperlawyer@gmail.com>
2019-12-14 15:03:56 +0100

    Solve day 12 part 2

--- 2019/Day12.java
+++ 2019/Day12.java
@@ -2,18 +2,36 @@ import java.util.Arrays;
 
 public class Day12 {
     public static void main(String[] args) {
-        Moon[] moons = new Moon[] {
-            new Moon(-10, -10, -13),
-            new Moon(5, 5, -9),
-            new Moon(3, 8, -16),
-            new Moon(1, 3, -3)
-        };
+        part1();
+        part2();
+    }
 
-        for (int i = 0; i < 1000; i++) step(moons);
+    private static void part1() {
+        Moon[] m = getInput();
 
-        int totalEnergy = Arrays.stream(moons).mapToInt(Moon::getEnergy).sum();
-        for (Moon m : moons) System.out.println(m);
+        for (int i = 0; i < 1000; i++) step(m);
+
+        int totalEnergy = Arrays.stream(m).mapToInt(Moon::getEnergy).sum();
         System.out.println("Total energy: " + totalEnergy);
+    }
+
+    private static void part2() {
+        Moon[] m1 = getInput();
+        Moon[] m2 = getInput();
+        int[] cycles = new int[3];
+        int steps = 1;
+
+        while (cycles[0] == 0 || cycles[1] == 0 || cycles[2] == 0) {
+            step(m2);
+            steps++;
+            for (int i = 0; i < 3; i++) {
+                if (cycles[i] == 0 && m1[0].dim[i] == m2[0].dim[i] && m1[1].dim[i] == m2[1].dim[i] && m1[2].dim[i] == m2[2].dim[i] && m1[3].dim[i] == m2[3].dim[i]) {
+                    cycles[i] = steps;
+                }
+            }
+        }
+
+        System.out.printf("Use WolframAlpha: lcm(%d,%d,%d)", cycles[0], cycles[1], cycles[2]);
     }
 
     private static void step(Moon[] moons) {
@@ -31,6 +49,15 @@ public class Day12 {
                 m.dim[i] += m.vel[i];
             }
         }
+    }
+
+    private static Moon[] getInput() {
+        return new Moon[] {
+            new Moon(-10, -10, -13),
+            new Moon(5, 5, -9),
+            new Moon(3, 8, -16),
+            new Moon(1, 3, -3)
+        };
     }
 
     private static class Moon {
