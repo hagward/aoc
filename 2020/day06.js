d18e24ffb8a16643d11744cb7e0ffffb654a811f
Anders Hagward <taperlawyer@gmail.com>
2020-12-06 11:04:58 +0100

    Solve day 6

---
+++ 2020/day06.js
@@ -0,0 +1,21 @@
+const numAnswersPart1 = (s) => new Set(groupAnswers(s)).size;
+
+const numAnswersPart2 = (s) => {
+  const numPersons = s.split("\n").length;
+  const answers = groupAnswers(s).reduce((p, v) => {
+    p[v] = p[v] ? p[v] + 1 : 1;
+    return p;
+  }, {});
+  return Object.values(answers).filter((a) => a === numPersons).length;
+};
+
+const groupAnswers = (s) => s.split("").filter((c) => c !== "\n");
+
+const sum = (arr, f) => arr.reduce((p, v) => p + f(v), 0);
+
+const fs = require("fs");
+const input = fs.readFileSync("day06.txt", "utf8").trim();
+const groups = input.split("\n\n");
+
+console.log(sum(groups, numAnswersPart1));
+console.log(sum(groups, numAnswersPart2));
