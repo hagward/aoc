b50a1e40ded57cfb549f70347fb05ab90aba9f16
Anders Hagward <taperlawyer@gmail.com>
2021-01-06 17:08:20 +0100

    Solve day 11 part 2

--- 2020/day11.js
+++ 2020/day11.js
@@ -1,62 +1,97 @@
 const fs = require("fs");
-const v = fs
+
+const INPUT = fs
   .readFileSync("inputs/day11.txt", "utf8")
   .trim()
   .split("\n")
   .map((line) => line.split(""));
 
-// const v = [
-//   "L.LL.LL.LL",
-//   "LLLLLLL.LL",
-//   "L.L.L..L..",
-//   "LLLL.LL.LL",
-//   "L.LL.LL.LL",
-//   "L.LLLLL.LL",
-//   "..L.L.....",
-//   "LLLLLLLLLL",
-//   "L.LLLLLL.L",
-//   "L.LLLLL.LL",
-// ].map((line) => line.split(""));
+const DIRS = [
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  [-1, 0],
+  [1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+];
 
-const isEmpty = (i, j) => v[i] && v[i][j] === "L";
-const isOccupied = (i, j) => v[i] && v[i][j] === "#";
+const FLOOR = ".";
+const EMPTY = "L";
+const OCCUPIED = "#";
 
-const numAdjacent = (i, j, f) =>
-  [
-    [i - 1, j - 1],
-    [i - 1, j],
-    [i - 1, j + 1],
-    [i, j - 1],
-    [i, j + 1],
-    [i + 1, j - 1],
-    [i + 1, j],
-    [i + 1, j + 1],
-  ].filter(([i, j]) => f(i, j)).length;
+const findNeighbor = (layout, x, y, dx, dy) => {
+  while (true) {
+    x += dx;
+    y += dy;
 
-while (true) {
-  const changes = [];
-  for (let i = 0; i < v.length; i++) {
-    for (let j = 0; j < v[i].length; j++) {
-      if (isEmpty(i, j) && numAdjacent(i, j, isOccupied) === 0) {
-        changes.push({ i, j, change: "#" });
-      } else if (isOccupied(i, j) && numAdjacent(i, j, isOccupied) >= 4) {
-        changes.push({ i, j, change: "L" });
+    if (!layout[y]) return;
+
+    switch (layout[y][x]) {
+      case EMPTY:
+      case OCCUPIED:
+      case undefined:
+        return layout[y][x];
+    }
+  }
+};
+
+const simulate = (calcChange) => {
+  const layout = INPUT.map((line) => line.slice());
+
+  while (true) {
+    const changes = [];
+
+    for (let i = 0; i < layout.length; i++) {
+      for (let j = 0; j < layout[i].length; j++) {
+        const change = calcChange(layout, i, j);
+        if (change) changes.push({ i, j, change });
       }
+    }
+
+    if (!changes.length) break;
+
+    for (const change of changes) {
+      layout[change.i][change.j] = change.change;
     }
   }
 
-  if (!changes.length) break;
+  return layout;
+};
 
-  for (let change of changes) {
-    v[change.i][change.j] = change.change;
-  }
-}
+const partOne = () =>
+  simulate((layout, i, j) => {
+    const seat = layout[i][j];
+    if (seat === FLOOR) return;
 
-let numOccupied = 0;
-for (let i = 0; i < v.length; i++) {
-  for (let j = 0; j < v[i].length; j++) {
-    if (isOccupied(i, j)) numOccupied++;
-  }
-}
+    const numOccupied = DIRS.map(
+      ([dx, dy]) => layout[i + dy] && layout[i + dy][j + dx]
+    ).filter((s) => s === OCCUPIED).length;
 
-console.log(numOccupied);
+    if (seat === EMPTY && numOccupied === 0) {
+      return OCCUPIED;
+    } else if (seat === OCCUPIED && numOccupied >= 4) {
+      return EMPTY;
+    }
+  });
+
+const partTwo = () =>
+  simulate((layout, i, j) => {
+    const seat = layout[i][j];
+    if (seat === FLOOR) return;
+
+    const numOccupied = DIRS.map(([dx, dy]) =>
+      findNeighbor(layout, j, i, dx, dy)
+    ).filter((s) => s === OCCUPIED).length;
+
+    if (seat === EMPTY && numOccupied === 0) {
+      return OCCUPIED;
+    } else if (seat === OCCUPIED && numOccupied >= 5) {
+      return EMPTY;
+    }
+  });
+
+[partOne(), partTwo()]
+  .map((layout) => layout.flat().filter((seat) => seat === OCCUPIED).length)
+  .forEach((numOccupied) => console.log(numOccupied));
