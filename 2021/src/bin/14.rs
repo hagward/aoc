efef10f56a6f63072cae6fc289308ffc7d7d8bfe
Anders Hagward <taperlawyer@gmail.com>
2021-12-14 18:36:11 +0100

    Solve day 14 part 2

--- 2021/src/bin/14.rs
+++ 2021/src/bin/14.rs
@@ -1,62 +1,42 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 
 static INPUT: &str = "CKKOHNSBPCPCHVNKHFFK\n\nKO -> C\nSO -> S\nBF -> V\nVN -> B\nOV -> K\nVH -> O\nKV -> N\nKB -> F\nNB -> C\nHS -> K\nPF -> B\nHB -> N\nOC -> H\nFS -> F\nVV -> S\nKF -> C\nFN -> F\nKP -> S\nHO -> N\nNH -> K\nOO -> S\nFB -> C\nBP -> F\nCH -> N\nSN -> O\nKN -> B\nCV -> O\nCC -> B\nVB -> C\nPH -> V\nCO -> K\nKS -> K\nBK -> N\nFH -> S\nPV -> H\nCB -> P\nFO -> F\nBB -> K\nOB -> C\nHH -> F\nON -> O\nFK -> B\nNF -> F\nSV -> F\nCP -> H\nSS -> B\nOP -> H\nNS -> O\nHK -> N\nBC -> P\nNV -> V\nVS -> F\nPC -> V\nCS -> F\nNP -> V\nPS -> F\nVC -> F\nKK -> S\nPO -> P\nHF -> H\nKC -> P\nSF -> N\nBV -> N\nFF -> V\nFV -> V\nBO -> N\nOS -> C\nOF -> H\nCN -> S\nNO -> O\nNC -> B\nVK -> C\nHN -> B\nPK -> N\nSK -> S\nHV -> F\nBH -> B\nOK -> S\nVO -> B\nBS -> H\nPP -> N\nSC -> K\nBN -> P\nFC -> S\nSB -> B\nSH -> H\nNN -> V\nNK -> N\nVF -> H\nCF -> F\nPB -> C\nSP -> P\nKH -> C\nVP -> N\nCK -> H\nHP -> P\nFP -> B\nHC -> O\nPN -> F\nOH -> H";
 
-/*
-static INPUT: &str = "NNCB
-
-CH -> B
-HH -> N
-CB -> H
-NH -> C
-HB -> C
-HC -> B
-HN -> C
-NN -> C
-BH -> H
-NC -> B
-NB -> B
-BN -> B
-BB -> N
-BC -> B
-CC -> N
-CN -> C";
-*/
-
-fn solve(template: String, rules: &HashMap<&str, &str>, iterations: usize) -> u64 {
-    let mut count = HashMap::new();
-    let mut q1: VecDeque<String> = VecDeque::new();
-    let mut q2: VecDeque<String> = VecDeque::new();
-    for i in 0..template.len() {
-        *count.entry(&template[i..i + 1]).or_insert(0) += 1;
-        if i != template.len() - 1 {
-            q1.push_back((&template[i..i + 2]).to_string());
-        }
+fn solve(template: &str, rules: &HashMap<(&str, &str), &str>, iterations: usize) -> u64 {
+    let len = template.len();
+    let mut pair_count = HashMap::new();
+    for i in 0..len - 1 {
+        *pair_count
+            .entry((&template[i..i + 1], &template[i + 1..i + 2]))
+            .or_insert(0) += 1;
     }
     for _ in 0..iterations {
-        let (q_full, q_empty) = if q1.is_empty() {
-            (&mut q2, &mut q1)
-        } else {
-            (&mut q1, &mut q2)
-        };
-        while let Some(pair) = q_full.pop_front() {
-            if let Some(letter) = rules.get(pair.as_str()) {
-                *count.entry(letter).or_insert(0) += 1;
-                q_empty.push_back(format!("{}{}", &pair[0..1], letter));
-                q_empty.push_back(format!("{}{}", letter, &pair[1..2]));
+        for ((a, b), count) in pair_count.clone() {
+            if let Some(c) = rules.get(&(a, b)) {
+                *pair_count.entry((a, c)).or_insert(0) += count;
+                *pair_count.entry((c, b)).or_insert(0) += count;
+                *pair_count.get_mut(&(a, b)).unwrap() -= count;
             }
         }
     }
-    count.values().max().unwrap() - count.values().min().unwrap()
+    let mut char_count = HashMap::new();
+    for (&(a, _), count) in &pair_count {
+        *char_count.entry(a).or_insert(0) += count;
+    }
+    *char_count.get_mut(&template[len - 1..len]).unwrap() += 1;
+    char_count.values().max().unwrap() - char_count.values().min().unwrap()
 }
 
 fn main() {
     let mut lines = INPUT.lines();
     let template = lines.next().unwrap();
-    let rules: HashMap<&str, &str> = lines
+    let rules: HashMap<(&str, &str), &str> = lines
         .skip(1)
-        .map(|line| line.split_once(" -> ").unwrap())
+        .map(|line| {
+            let (from, to) = line.split_once(" -> ").unwrap();
+            ((&from[0..1], &from[1..2]), to)
+        })
         .collect();
-    println!("Part one: {}", solve(template.to_string(), &rules, 10));
-    // println!("Part two: {}", solve(template.to_string(), &rules, 40));
+    println!("Part one: {}", solve(&template, &rules, 10));
+    println!("Part two: {}", solve(&template, &rules, 40));
 }
