7084482d30c4596b2c51cffc9952d0d055ee26d8
Anders Hagward <taperlawyer@gmail.com>
2024-12-02 08:33:39 +0100

    Solve day 2 part 2

--- 2024/src/bin/02.rs
+++ 2024/src/bin/02.rs
@@ -3,29 +3,51 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("src/bin/02.txt").unwrap();
 
-    let part1 = input
+    let reports: Vec<Vec<i32>> = input
         .lines()
-        .filter(|line| {
-            let numbers: Vec<i32> = line
-                .split_ascii_whitespace()
+        .map(|line| {
+            line.split_ascii_whitespace()
                 .map(|s| s.parse::<i32>().unwrap())
-                .collect();
-            for i in 0..numbers.len() - 1 {
-                let a = numbers[i];
-                let b = numbers[i + 1];
-                if a.abs_diff(b) < 1 || a.abs_diff(b) > 3 {
-                    return false;
-                }
-                if i < numbers.len() - 2 {
-                    let c = numbers[i + 2];
-                    if (a - b).signum() != (b - c).signum() {
-                        return false;
-                    }
+                .collect()
+        })
+        .collect();
+
+    let part1 = reports.iter().filter(|&levels| is_safe(levels)).count();
+
+    let part2 = reports
+        .iter()
+        .filter(|&levels| {
+            if is_safe(levels) {
+                return true;
+            }
+            for i in 0..levels.len() {
+                let mut levels_minus_one = levels.to_vec();
+                levels_minus_one.remove(i);
+                if is_safe(&levels_minus_one) {
+                    return true;
                 }
             }
-            true
+            false
         })
         .count();
 
     println!("{}", part1);
+    println!("{}", part2);
+}
+
+fn is_safe(levels: &[i32]) -> bool {
+    for i in 0..levels.len() - 1 {
+        let a = levels[i];
+        let b = levels[i + 1];
+        if a.abs_diff(b) < 1 || a.abs_diff(b) > 3 {
+            return false;
+        }
+        if i < levels.len() - 2 {
+            let c = levels[i + 2];
+            if (a - b).signum() != (b - c).signum() {
+                return false;
+            }
+        }
+    }
+    true
 }
