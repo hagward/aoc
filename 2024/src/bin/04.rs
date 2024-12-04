aa3e8eea195e780ca159dba649a30d2878adb875
Anders Hagward <taperlawyer@gmail.com>
2024-12-04 20:53:08 +0100

    Solve day 4 part 2

--- 2024/src/bin/04.rs
+++ 2024/src/bin/04.rs
@@ -7,7 +7,8 @@ fn main() {
         .map(|line| line.chars().collect())
         .collect();
 
-    let mut count = 0;
+    let mut part_1 = 0;
+    let mut part_2 = 0;
     for y in 0..input.len() {
         for x in 0..input[y].len() {
             for (dx, dy) in [
@@ -21,21 +22,37 @@ fn main() {
                 (-1, -1),
             ] {
                 if has_xmas(&input, x, y, dx, dy) {
-                    count += 1;
+                    part_1 += 1;
                 }
+            }
+
+            if (has_mas(&input, x, y, 1, 1) || has_mas(&input, x + 2, y + 2, -1, -1))
+                && (has_mas(&input, x + 2, y, -1, 1) || has_mas(&input, x, y + 2, 1, -1))
+            {
+                part_2 += 1;
             }
         }
     }
 
-    println!("{}", count);
+    println!("{}", part_1);
+    println!("{}", part_2);
 }
 
-fn has_xmas(input: &[Vec<char>], start_x: usize, start_y: usize, dx: i32, dy: i32) -> bool {
-    let xmas = &['X', 'M', 'A', 'S'];
-    for i in 0..4 {
-        let x = (start_x as i32 + dx * i as i32) as usize;
-        let y = (start_y as i32 + dy * i as i32) as usize;
-        if y >= input.len() || x >= input[y].len() || input[y][x] != xmas[i] {
+fn has_xmas(input: &[Vec<char>], xs: usize, ys: usize, dx: i32, dy: i32) -> bool {
+    let needle = &['X', 'M', 'A', 'S'];
+    has_string(needle, input, xs, ys, dx, dy)
+}
+
+fn has_mas(input: &[Vec<char>], xs: usize, ys: usize, dx: i32, dy: i32) -> bool {
+    let needle = &['M', 'A', 'S'];
+    has_string(needle, input, xs, ys, dx, dy)
+}
+
+fn has_string(s: &[char], input: &[Vec<char>], xs: usize, ys: usize, dx: i32, dy: i32) -> bool {
+    for i in 0..s.len() {
+        let x = (xs as i32 + dx * i as i32) as usize;
+        let y = (ys as i32 + dy * i as i32) as usize;
+        if y >= input.len() || x >= input[y].len() || input[y][x] != s[i] {
             return false;
         }
     }
