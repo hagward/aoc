1f533be9c5d7cf96d2d23d235013d77c98b01213
Anders Hagward <taperlawyer@gmail.com>
2024-12-08 17:57:18 +0100

    Solve day 8 part 2

--- 2024/day08.nim
+++ 2024/day08.nim
@@ -4,42 +4,59 @@ let grid = readAll(stdin).strip.splitLines.mapIt(it.toSeq)
 
 type Pos = tuple[x: int, y: int]
 
-proc getAntinodes(a, b: Pos): (Pos, Pos) =
+proc inBounds(pos: Pos): bool =
+    return pos.x >= 0 and pos.x < grid[0].len and
+           pos.y >= 0 and pos.y < grid.len
+
+proc getTwoAntinodes(a, b: Pos): seq[Pos] =
     let
         dx = abs(a.x - b.x)
         dy = abs(a.y - b.y)
         k = (a.y - b.y) / (a.x - b.x)
     if k >= 0:
-        return ((x: min(a.x, b.x) - dx, y: min(a.y, b.y) - dy),
-                (x: max(a.x, b.x) + dx, y: max(a.y, b.y) + dy))
+        return @[(x: min(a.x, b.x) - dx, y: min(a.y, b.y) - dy),
+                 (x: max(a.x, b.x) + dx, y: max(a.y, b.y) + dy)].filter(inBounds)
     else:
-        return ((x: min(a.x, b.x) - dx, y: max(a.y, b.y) + dy),
-                (x: max(a.x, b.x) + dx, y: min(a.y, b.y) - dy))
+        return @[(x: min(a.x, b.x) - dx, y: max(a.y, b.y) + dy),
+                 (x: max(a.x, b.x) + dx, y: min(a.y, b.y) - dy)].filter(inBounds)
 
-proc inBounds(pos: Pos): bool =
-    return pos.x >= 0 and pos.x < grid[0].len and
-           pos.y >= 0 and pos.y < grid.len
+proc getAllAntinodes(a, b: Pos): seq[Pos] =
+    let
+        dx = abs(a.x - b.x)
+        dy = abs(a.y - b.y)
+        k = (a.y - b.y) / (a.x - b.x)
+    var
+        x = a.x
+        y = a.y
+        antinodes: seq[Pos]
+    while inBounds((x, y)):
+        antinodes.add((x, y))
+        x += dx
+        if k >= 0: y += dy else: y -= dy
+    x = a.x - dx
+    if k >= 0: y = a.y - dy else: y = a.y + dy
+    while inBounds((x, y)):
+        antinodes.add((x, y))
+        x -= dx
+        if k >= 0: y -= dy else: y += dy
+    return antinodes
 
-var table = newTable[char, seq[Pos]]()
+var antennasByFrequency = newTable[char, seq[Pos]]()
 for y in 0..<grid.len:
     for x in 0..<grid[0].len:
         let c = grid[y][x]
         if c == '.': continue
-        table.mgetOrPut(c, @[]).add((x, y))
+        antennasByFrequency.mgetOrPut(c, @[]).add((x, y))
 
-var antinodes: HashSet[Pos]
-for c, nodes in table:
+var
+    antinodesPart1: HashSet[Pos]
+    antinodesPart2: HashSet[Pos]
+for c, nodes in antennasByFrequency:
     for i in 0..<nodes.len-1:
         for j in i+1..<nodes.len:
-            let (a, b) = getAntinodes(nodes[i], nodes[j])
-            if inBounds(a): antinodes.incl(a)
-            if inBounds(b): antinodes.incl(b)
-echo antinodes.len
-
-# for i in 0..<grid.len:
-#     for j in 0..<grid[0].len:
-#         if antinodes.contains((x: j, y: i)):
-#             stdout.write '#'
-#         else:
-#             stdout.write grid[i][j]
-#     echo ""
+            for node in getTwoAntinodes(nodes[i], nodes[j]):
+                antinodesPart1.incl(node)
+            for node in getAllAntinodes(nodes[i], nodes[j]):
+                antinodesPart2.incl(node)
+echo antinodesPart1.len
+echo antinodesPart2.len
