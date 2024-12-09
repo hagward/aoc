062c5fe8ac2921ba07cb90f55fda5898f5abfb4b
Anders Hagward <taperlawyer@gmail.com>
2024-12-09 22:05:46 +0100

    Solve day 9 part 2

--- 2024/day09.nim
+++ 2024/day09.nim
@@ -1,40 +1,53 @@
 import std/[math, sequtils, strutils, sugar]
 
-let disk = readAll(stdin).strip.map(it => int(it) - int('0'))
+type NodeType = enum File, Free
+type Node = tuple[nodeType: NodeType, size: int, id: int]
 
-var expanded: seq[int]
-for i in 0..<disk.len:
-    for j in 0..<disk[i]:
+var input = readAll(stdin).strip.map(it => int(it) - int('0'))
+
+var
+    disk: seq[int]
+    nodes: seq[Node]
+for i in 0..<input.len:
+    let nodeType = if i mod 2 == 0: File else: Free
+    nodes.add((nodeType, input[i], i.floorDiv(2)))
+    for j in 0..<input[i]:
         if i mod 2 == 0:
-            expanded.add(i.floorDiv(2))
+            disk.add(i.floorDiv(2))
         else:
-            expanded.add(-1)
-
-# for e in expanded:
-#     if e == -1:
-#         stdout.write "."
-#     else:
-#         stdout.write e
-# echo ""
+            disk.add(-1)
 
 var freeI = 0
-for i in countdown(expanded.len-1, 0):
-    if expanded[i] == -1: continue
-    while freeI < i and expanded[freeI] != -1:
+for i in countdown(disk.len-1, 0):
+    if disk[i] == -1: continue
+    while freeI < i and disk[freeI] != -1:
         freeI += 1
     if freeI < i:
-        expanded[freeI] = expanded[i]
-        expanded[i] = -1
-
-# for e in expanded:
-#     if e == -1:
-#         stdout.write "."
-#     else:
-#         stdout.write e
-# echo ""
+        disk[freeI] = disk[i]
+        disk[i] = -1
 
 var sum = 0
-for i in 0..<expanded.len:
-    if expanded[i] == -1: continue
-    sum += i*expanded[i]
+for i in 0..<disk.len:
+    if disk[i] == -1: continue
+    sum += i*disk[i]
 echo sum
+
+for i in 0..<nodes.len:
+    if nodes[i].nodeType != Free: continue
+    for j in countdown(nodes.len-1, i+1):
+        if nodes[j].nodeType != File: continue
+        if nodes[j].size > nodes[i].size: continue
+        nodes.insert(nodes[j], i)
+        nodes[j+1].nodeType = Free
+        nodes[i+1].size -= nodes[i].size
+        break
+
+var
+    sum2 = 0
+    index = 0
+for (nodeType, size, id) in nodes:
+    if nodeType == File:
+        for i in index..<index+size:
+            sum2 += i * id
+    index += size
+echo sum2
