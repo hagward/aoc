1478ba55a88767c75a2e19176b3b97e8dd8286a1
Anders Hagward <taperlawyer@gmail.com>
2024-12-19 11:52:06 +0100

    Solve day 19 part 2

--- 2024/day19.nim
+++ 2024/day19.nim
@@ -1,4 +1,4 @@
-import std/strutils
+import std/[strutils, tables]
 
 let input = readAll(stdin).strip.split("\n\n")
 let patterns = input[0].split(", ")
@@ -14,8 +14,28 @@ proc check(current: string, target: string, patterns: seq[string]): bool =
                 return true
     return false
 
-var sum = 0
+var cache: Table[(string, string), int]
+proc countWays(current: string, target: string, patterns: seq[string]): int =
+    let key = (current, target)
+    if key in cache:
+        return cache[key]
+    if current == target:
+        return 1
+    var sum = 0
+    for i in 0..<patterns.len:
+        let newCurrent = current & patterns[i]
+        if target.startsWith(newCurrent):
+            let ways = countWays(newCurrent, target, patterns)
+            cache[(newCurrent, target)] = ways
+            sum += ways
+        else:
+            cache[(newCurrent, target)] = 0
+    return sum
+
+var sum1, sum2: int
 for design in designs:
     if check("", design, patterns):
-        sum += 1
-echo sum
+        sum1 += 1
+    sum2 += countWays("", design, patterns)
+echo sum1
+echo sum2
