080c2fc99f085c2107e1af5bb215a0d630f6c21d
Anders Hagward <taperlawyer@gmail.com>
2024-12-23 15:43:18 +0100

    Solve day 23 part 2

--- 2024/day23.nim
+++ 2024/day23.nim
@@ -2,20 +2,38 @@ import std/[algorithm, sequtils, sets, strutils, sugar, tables]
 
 let input = readAll(stdin).splitLines
 
-var graph: Table[string, HashSet[string]]
+var neighbors: Table[string, HashSet[string]]
 for line in input:
     let (a, b) = (line[0..1], line[3..4])
-    if not graph.contains(a): graph[a] = initHashSet[string]()
-    if not graph.contains(b): graph[b] = initHashSet[string]()
-    graph[a].incl(b)
-    graph[b].incl(a)
+    if not neighbors.contains(a): neighbors[a] = initHashSet[string]()
+    if not neighbors.contains(b): neighbors[b] = initHashSet[string]()
+    neighbors[a].incl(b)
+    neighbors[b].incl(a)
 
 var groups: HashSet[(string, string, string)]
-for a in graph.keys:
-    for b in graph[a]:
-        for c in graph[b]:
-            if c in graph[a]:
+for a in neighbors.keys:
+    for b in neighbors[a]:
+        for c in neighbors[b]:
+            if c in neighbors[a]:
                 let group = sorted(@[a, b, c])
                 groups.incl((group[0], group[1], group[2]))
 
 echo groups.toSeq.filter(group => @[group[0], group[1], group[2]].any(node => node.startsWith("t"))).len
+
+var cliques: seq[HashSet[string]]
+
+proc bronKerbosch(r, p, x: HashSet[string]) =
+    if p.len == 0 and x.len == 0:
+        cliques.add(r)
+    var
+        rr = r
+        pp = p
+        xx = x
+    for v in p:
+        bronKerbosch(rr + [v].toHashSet, pp * neighbors[v], xx * neighbors[v])
+        pp.excl(v)
+        xx.incl(v)
+
+bronKerbosch(initHashSet[string](), neighbors.keys.toSeq.toHashSet, initHashSet[string]())
+
+echo sorted(cliques[cliques.mapIt(it.len).maxIndex].toSeq).join(",")
