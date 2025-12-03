866e8af196c9ba19f244dd67c0720cd13d611e59
Anders Hagward <taperlawyer@gmail.com>
2025-12-03 10:01:38 +0100

    Solve day 3 part 2

--- 2025/03.py
+++ 2025/03.py
@@ -1,13 +1,23 @@
 with open('input/03.txt') as f:
     lines = f.read().splitlines()
 
-def find_max(n_str):
-    max_digit = max(n_str[:-1])
-    max_index = n_str.index(max_digit)
-    max_digit_2 = max(n_str[max_index+1:])
-    return int(max_digit + max_digit_2)
+def find_max(n_str: str, k: int):
+    ret = ''
+    l = 0
+    r = len(n_str) - k
+    while len(ret) < k:
+        part = n_str[l:r+1]
+        max_digit = max(part)
+        max_index = part.index(max_digit)
+        ret += max_digit
+        l += max_index + 1
+        r += 1
+    return int(ret)
 
 ans = 0
+ans2 = 0
 for line in lines:
-    ans += find_max(line)
+    ans += find_max(line, 2)
+    ans2 += find_max(line, 12)
 print(ans)
+print(ans2)
