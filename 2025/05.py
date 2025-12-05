11a74b565a51f997f7d2373c3956a276aa20aea8
Anders Hagward <taperlawyer@gmail.com>
2025-12-05 11:10:56 +0100

    Solve day 5 part 2

--- 2025/05.py
+++ 2025/05.py
@@ -3,19 +3,35 @@ with open('input/05.txt') as f:
 
 ranges = []
 for r in ranges_raw:
-    [min, max] = map(int, r.split('-'))
-    ranges.append([min, max])
+    [l, r] = map(int, r.split('-'))
+    ranges.append([l, r])
 
 ingredients = list(map(int, ingredients_raw))
 
 def in_range(ingredient):
-    for [min, max] in ranges:
-        if ingredient >= min and ingredient <= max:
+    for [l, r] in ranges:
+        if ingredient >= l and ingredient <= r:
             return True
     return False
 
-ans = 0
-for ingredient in ingredients:
-    if in_range(ingredient):
-        ans += 1
-print(ans)
+print(sum(1 for ingredient in ingredients if in_range(ingredient)))
+
+def merge_once():
+    for i in range(len(ranges) - 1):
+        for j in range(i + 1, len(ranges)):
+            [l1, r1] = ranges[i]
+            [l2, r2] = ranges[j]
+            if l2 <= l1 and r2 >= l1:
+                ranges[i] = [l2, max(r1, r2)]
+                ranges.pop(j)
+                return True
+            elif l1 <= l2 and r1 >= l2:
+                ranges[i] = [l1, max(r1, r2)]
+                ranges.pop(j)
+                return True
+    return False
+
+while merge_once():
+    pass
+
+print(sum(r - l + 1 for [l, r] in ranges))
