37eb8c8f6ac8cc6adc0de105c1d9bd6c04b2b9aa
Anders Hagward <taperlawyer@gmail.com>
2025-12-10 11:52:54 +0100

    Solve day 10 part 1

---
+++ 2025/10.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+with open('input/10.txt') as f:
+    lines = f.read().splitlines()
+
+def press_button(lights, button):
+    result = list(lights)
+    for i in button:
+        if result[i] == '.':
+            result[i] = '#'
+        else:
+            result[i] = '.'
+    return ''.join(result)
+
+def min_button_presses(goal, buttons):
+    visited = set()
+    q = deque([(0, goal.replace('#', '.'))])
+    while q:
+        (dist, node) = q.popleft()
+        if node == goal:
+            return dist
+        if node not in visited:
+            visited.add(node)
+            for button in buttons:
+                new_node = press_button(node, button)
+                if new_node not in visited:
+                    q.append((dist + 1, new_node))
+
+ans = 0
+for line in lines:
+    splitted = line.split()
+    goal = splitted[0][1:-1]
+    buttons = [list(map(int, s[1:-1].split(','))) for s in splitted[1:-1]]
+    joltage = splitted[-1]
+    ans += min_button_presses(goal, buttons)
+print(ans)
