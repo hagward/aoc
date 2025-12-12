b8ce8cc980c7d9825465c5f6a116a4575754d9e4
Anders Hagward <taperlawyer@gmail.com>
2025-12-12 19:02:31 +0100

    Solve day 6 part 2

--- 2025/06.py
+++ 2025/06.py
@@ -1,13 +1,39 @@
-with open('input/06.txt') as f:
-    lines = [line.split() for line in f.read().splitlines()]
+with open("input/06.txt") as f:
+    lines = f.read().splitlines()
 
-answers = [int(n) for n in lines[0]]
+splitted = [line.split() for line in lines]
+rows = len(splitted)
+cols = len(splitted[0])
 
-for i in range(1, len(lines) - 1):
-    for j in range(len(lines[i])):
-        if lines[len(lines) - 1][j] == '+':
-            answers[j] += int(lines[i][j])
+answers = [int(n) for n in splitted[0]]
+for i in range(1, rows - 1):
+    for j in range(cols):
+        if splitted[rows - 1][j] == "+":
+            answers[j] += int(splitted[i][j])
         else:
-            answers[j] *= int(lines[i][j])
-
+            answers[j] *= int(splitted[i][j])
 print(sum(answers))
+
+column_widths = [0 for _ in range(cols)]
+for row in splitted:
+    for i in range(cols):
+        if len(row[i]) > column_widths[i]:
+            column_widths[i] = len(row[i])
+
+ans2 = 0
+start = 0
+for i in range(cols):
+    is_add = lines[rows - 1][start] == "+"
+    part = 0 if is_add else 1
+    for j in range(column_widths[i]):
+        n = ""
+        for k in range(rows - 1):
+            if start + j < len(lines[k]):
+                n += lines[k][start + j]
+        if is_add:
+            part += int(n.strip())
+        else:
+            part *= int(n.strip())
+    ans2 += part
+    start += column_widths[i] + 1
+print(ans2)
